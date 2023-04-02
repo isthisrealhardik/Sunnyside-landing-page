fe2156b69a4afe1a9b3f98651ebbc6e0ab1e4589
Hardik <singhhardik6014@gmail.com>
2023-04-02 18:26:48 +0530

    footer fixed

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,23 +1,25 @@
 const Footer = () => {
     return (
-        <div className=" text-dark-mod-cyan space-y-12 bg-light-mod-cyan w-screen h-screen flex-col flex items-center justify-start">
-            <div className="grid grid-cols-2 grid-rows-2">
+        <div className=" text-dark-mod-cyan bg-light-mod-cyan w-screen h-screen flex-col flex items-center justify-start">
+            <div className="h-1/2 grid grid-cols-2 grid-rows-2">
                 <img className="" src="/images/mobile/image-gallery-cone.jpg" alt="Cone" />
                 <img className=" col-start-1 row-start-1" src="/images/mobile/image-gallery-milkbottles.jpg" alt="Milkbottles" />
                 <img className=" col-start-2 row-start-1" src="/images/mobile/image-gallery-orange.jpg" alt="Orange" />
                 <img className="" src="/images/mobile/image-gallery-sugar-cubes.jpg" alt="Sugar Cubes" />
             </div>
-            <h1 className="text-4xl">Sunnyside</h1>
-            <div className="flex space-x-6">
-                <h3>About</h3>
-                <h3>Services</h3>
-                <h3>Projects</h3>
-            </div>
-            <div className="flex space-x-4">
-                <img src="/images/icon-facebook.svg" alt="Facebook" />
-                <img src="/images/icon-instagram.svg" alt="Instagram" />
-                <img src="/images/icon-pinterest.svg" alt="Pinterest" />
-                <img src="/images/icon-twitter.svg" alt="Twitter" />
+            <div className=" h-1/2 flex flex-col space-y-12 justify-center items-center">
+                <h1 className="text-4xl">Sunnyside</h1>
+                <div className="flex space-x-6">
+                    <h3>About</h3>
+                    <h3>Services</h3>
+                    <h3>Projects</h3>
+                </div>
+                <div className="flex space-x-4">
+                    <img src="/images/icon-facebook.svg" alt="Facebook" />
+                    <img src="/images/icon-instagram.svg" alt="Instagram" />
+                    <img src="/images/icon-pinterest.svg" alt="Pinterest" />
+                    <img src="/images/icon-twitter.svg" alt="Twitter" />
+                </div>
             </div>
         </div>
     )
--- src/ThirdComp.jsx
+++ src/ThirdComp.jsx
@@ -2,7 +2,7 @@ const ThirdComp = ({ image, heading, paragraph, color }) => {
     let textColor = `flex flex-col justify-end items-center px-8 h-screen ${color}`
     return(
         <div id='text' className={textColor}>
-            <img className="h-screen absolute z-0" src={image} alt="Graphic Design" />
+            <img className="h-screen absolute z-0 object-cover" src={image} alt="Graphic Design" />
             <h1 className="z-10 text-center text-3xl pb-6">{heading}</h1>
             <p className="z-10 font-barlow text-base text-center pb-14">{paragraph}</p>
       </div>
