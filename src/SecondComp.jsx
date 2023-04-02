c2490fdfda730958fc8ad46a87dc7812515998b6
Hardik <singhhardik6014@gmail.com>
2023-04-02 18:30:08 +0530

    second component fixed

--- src/SecondComp.jsx
+++ src/SecondComp.jsx
@@ -1,10 +1,10 @@
 const SecondComp = ({ image, heading, paragraph, color }) => {
     return (
         <div className=" bg-white w-screen h-screen text-very-dark-desat-blue flex flex-col justify-start items-center">
-            <img className="w-full object-cover" src={image} alt="Image Transform" />
-            <div className="flex flex-col justify-center items-center px-8">
-                <h1 className=" text-3xl font-black text-center mt-8">{heading}</h1>
-                <p className=" text-base font-barlow my-6 text-very-dark-grayish-blue text-center">{paragraph}</p>
+            <img className="h-1/2 w-full object-cover" src={image} alt="Image Transform" />
+            <div className="h-1/2 flex flex-col justify-center items-center px-8">
+                <h1 className=" text-3xl font-black text-center">{heading}</h1>
+                <p className=" text-base font-barlow text-very-dark-grayish-blue text-center my-6">{paragraph}</p>
                 <button id="button" className="hover:text-white transition border-2 rounded-full border-solid border-opacity-50 bg-transparent">Learn More</button>
             </div>
       </div>
